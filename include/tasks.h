f16c47f3dbc2e2810e51456e7999c3897afbee1b
Notexxx <artemkrasavin1337@gmail.com>
2026-06-07 15:05:26 +0300

    еще оаз исправил

--- include/tasks.h
+++ include/tasks.h
@@ -2,10 +2,7 @@
 #ifndef INCLUDE_TASKS_H_
 #define INCLUDE_TASKS_H_
 
-// Возвращает зазор между верёвкой и землёй (в метрах)
 double earthRopeGap();
-
-// Возвращает общую стоимость материалов для бассейна (в рублях)
 double poolCost();
 
 #endif  // INCLUDE_TASKS_H_
